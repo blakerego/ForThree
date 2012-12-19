892332c280a262ca231b6d16e7b8216452bc3cb5
blakerego <blake.rego@gmail.com>
2012-12-18 23:32:49 -0500

    added ability to parse a team's weekly statistics. I am assuming the user is pasting in data from the yahoo team app.

--- ForThree/Form1.cs
+++ ForThree/Form1.cs
@@ -6,14 +6,22 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ForThree.Implementations;
 
 namespace ForThree
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TeamWeekParser mTeamWeekInput = new TeamWeekParser(textBox1.Text);
+            mTeamWeekInput.Fetch(); 
         }
     }
 }
--- ForThree/Interfaces/ITeamWeekInput.cs
+++ ForThree/Interfaces/ITeamWeekInput.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Domain;
 
 namespace ForThree.Implementations
 {
@@ -10,6 +11,6 @@ namespace ForThree.Implementations
     /// </summary>
     public interface ITeamWeekInput
     {
-        public void Fetch(); 
+        Team Fetch(); 
     }
 }
