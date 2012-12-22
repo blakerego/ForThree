3be3584b09ec8ec5eb1b20e1c821c3be1aef095b
Blake Rego <brego@greenchargenet.com>
2012-12-21 22:35:32 -0500

    trying a rails implementation of this app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForThree::Application.config.secret_token = '968da8fc544d32ff619a63f038d381b80a37ae312309b6dc926da99cec042f5b9de769a4b8a62012a2e64a025ed726a1eff4c4421bb364fe47b834ac65ac9696'
