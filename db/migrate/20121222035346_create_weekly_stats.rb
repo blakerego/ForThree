4f57cb396bb175acc4cc81edc4d9a069cd597c7d
Blake Rego <brego@greenchargenet.com>
2012-12-21 23:29:14 -0500

    fixing db migration table for weekly_stats

--- db/migrate/20121222035346_create_weekly_stats.rb
+++ db/migrate/20121222035346_create_weekly_stats.rb
@@ -3,9 +3,9 @@ class CreateWeeklyStats < ActiveRecord::Migration
     create_table :weekly_stats do |t|
       t.integer :team_id
       t.integer :week_number
-      t.double :fg%
-      t.double :ft%
-      t.integer :3ptm
+      t.float :field_goal_percentage
+      t.float :free_throw_percentage
+      t.integer :three_pointers_made
       t.integer :rebounds
       t.integer :assists
       t.integer :steals
