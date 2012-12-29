80bc475390c6ab5498199e073f7af7e17783d753
Charles <csm2115@gmail.com>
2012-12-29 02:31:30 -0500

    Corrected bad syntax in team.rb and started seed.rb by inserting the teams in there (only with team name)

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
 	has_many :weekly_stats
 	has_many :players
-  attr_accessible :name
+  attr_accessible :name, :point_guard_id, :shooting_guard_id, :guard_id, :power_forward_id, :small_forward_id, :forward_id, :center1_id, :center2_id, :util1_id, :util2_id, :bench1_id, :bench2_id, :bench3_id
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+team1 = Team.create(:name => 'KingJames')
+team2 = Team.create(:name => 'Gangster Clowns')
+team3 = Team.create(:name => 'The Real Beal')
+team4 = Team.create(:name => 'TheLarrySandersShow')
+team5 = Team.create(:name => 'UncleDrew')
+team6 = Team.create(:name => 'Heatles!')
+team7 = Team.create(:name => 'thee LAKE show')
+team8 = Team.create(:name => 'Ray-gun Allen')
+team9 = Team.create(:name => 'RUNDLC')
+team10 = Team.create(:name => 'Blakers')
+team11 = Team.create(:name => 'The Manimals')
+team12 = Team.create(:name => 'Victorious Secret')
+team13 = Team.create(:name => 'Legends of the Ball')
+team14 = Team.create(:name => 'Bosh.0')
+team15 = Team.create(:name => 'knicks2013champs')
+team16 = Team.create(:name => 'BALLER17')
