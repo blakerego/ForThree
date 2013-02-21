c6a4ef7a5d23ae61bcf709562bd422c5b2c0333d
Charles <csm2115@gmail.com>
2013-02-20 21:31:31 -0500

    Checking in some new credentials

--- DB_Updater/restcall.rb
+++ DB_Updater/restcall.rb
@@ -5,27 +5,28 @@ require 'oauth'
  
 o = OauthUtil.new
  
-o.consumer_key = 'dj0yJmk9SWVLZjV4cnBPY1FBJmQ9WVdrOVFVWTNOalJFTkdFbWNHbzlNVEkxTnpnNE5UYzJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yNw--';
-o.consumer_secret = '5ba1bca4d7fcdc74420a778127eb24bebd14d6dc';
+o.consumer_key = 'dj0yJmk9SHRQVVN5M1VlUFZsJmQ9WVdrOVFVWTNOalJFTkdFbWNHbzlNVEkxTnpnNE5UYzJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD03YQ--';
+o.consumer_secret = '251cfb9cc952cdf2ea47d24a4172a8eac2b4eb2b';
  
 url = 'http://query.yahooapis.com/v1/yql?q=select%20*%20from%20fantasysports.leagues%20where%20league_key%3D\'304.l.18720\'&diagnostics=true';
  
-#parsed_url = URI.parse( url )
+parsed_url = URI.parse( url )
 
 
 
 #http.use_ssl =  true;
-#Net::HTTP.start(parsed_url.host) do | http |
-#  req = Net::HTTP::Get.new(url.request_uri)
-#  response = http.request(req)
-#  print req
-#  print response.read_body
-#  
+Net::HTTP.start(parsed_url.host) do | http |
+  req = Net::HTTP::Get.new "#{ parsed_url.path }?#{ o.sign(parsed_url).query_string }"
+  response = http.request(req)
+  puts req
+  puts response.read_body
+end
+  
 
-@consumer=OAuth::Consumer.new o.consumer_key,
-							o.consumer_secret,
-							{:site=>"http://query.yahooapis.com"}
-
-@request_token = @consumer.get_request_token
-
-puts @request_token.authorize_url
+#@consumer=OAuth::Consumer.new o.consumer_key,
+#							o.consumer_secret,
+#							{:site=>"http://query.yahooapis.com"}
+#
+#@request_token = @consumer.get_request_token
+#
+#puts @request_token.authorize_url
